74a6c48c76f4ac06014e371ebedddfc589eeb62b
JustinAndrade <j.andrade.john@gmail.com>
2019-03-09 19:01:24 -0500

    MVP Refactoring

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -35,7 +35,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1 className="Header">React Wars</h1>
+        <div className="Header">
+          <h1 >React Wars</h1>
+        </div>
         <CharacterList starwarsChars={ this.state.starwarsChars } />
       </div>
     );
