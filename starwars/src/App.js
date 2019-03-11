b1a23f959c29307b8e64f9b243457485502232f6
JustinAndrade <j.andrade.john@gmail.com>
2019-03-11 18:40:28 -0400

    stretch added

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -13,7 +13,11 @@ class App extends Component {
   }
 
   componentDidMount() {
-    this.getCharacters('https://swapi.co/api/people/');
+    console.log(this.getCharacters('https://swapi.co/api/people/'));
+    console.log(this.getCharacters('https://swapi.co/api/people/?page=2'));
+    console.log(this.getCharacters('https://swapi.co/api/people/?page=3'));
+    console.log(this.getCharacters('https://swapi.co/api/people/?page=4'));
+    console.log(this.getCharacters('https://swapi.co/api/people/?page=5'));
   }
 
   getCharacters = URL => {
@@ -25,7 +29,9 @@ class App extends Component {
         return res.json();
       })
       .then(data => {
+        console.log(data);
         this.setState({ starwarsChars: data.results });
+        // would have to set up a button to switch between pages with prevState //
       })
       .catch(err => {
         throw new Error(err);
@@ -38,7 +44,7 @@ class App extends Component {
         <div className="Header">
           <h1 >React Wars</h1>
         </div>
-        <CharacterList starwarsChars={ this.state.starwarsChars } />
+        <CharacterList starwarsChars={ this.state.starwarsChars} />
       </div>
     );
   }
